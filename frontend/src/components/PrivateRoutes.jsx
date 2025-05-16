21a924d414c3bdcef8948b86112aa6f45663e0b6
Vinod-donur <vinoddonur6@gmail.com>
2025-05-16 20:21:12 +0530

    feat: add LoadingSpinner component and update PrivateRoute to use it; enhance DashboardPage with loading state and toast notifications on logout

--- frontend/src/components/PrivateRoutes.jsx
+++ frontend/src/components/PrivateRoutes.jsx
@@ -1,11 +1,12 @@
 // components/PrivateRoute.jsx
 import { Navigate } from "react-router-dom";
 import useAuthStore from "../store/AuthStore";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, user, isCheckingAuth } = useAuthStore();
 
-  if (isCheckingAuth) return <div>Loading...</div>;
+  if (isCheckingAuth) return <LoadingSpinner />;
 
   if (!isAuthenticated || !user?.isVerified) {
     return <Navigate to="/login" replace />;
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,12 +1,20 @@
 import { motion } from "framer-motion";
-import  useAuthStore from "../store/AuthStore";
+import useAuthStore from "../store/AuthStore";
 import { formatDate } from "../utils/date";
+import { Loader } from "lucide-react";
+import toast from "react-hot-toast";
 
 const DashboardPage = () => {
-  const { user, logout } = useAuthStore();
+  const { user, logout, isLoading } = useAuthStore();
 
-  const handleLogout = () => {
-    logout();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      toast.success("Logout successful!", { autoclose: 2000 });
+    } catch (error) {
+      console.error("Logout error:", error);
+      toast.error("Logout failed. Please try again.", { autoclose: 2000 });
+    }
   };
   return (
     <motion.div
@@ -14,35 +22,35 @@ const DashboardPage = () => {
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.9 }}
       transition={{ duration: 0.5 }}
-      className="max-w-md w-full mx-auto mt-10 p-8 bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800"
+      className="max-w-md w-full mx-auto mt-10 p-8 bg-gray-100 rounded-xl shadow-2xl"
     >
-      <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-600 text-transparent bg-clip-text">
+      <h2 className="text-3xl font-bold mb-6 text-center text-gray-900 ">
         Dashboard
       </h2>
 
       <div className="space-y-6">
         <motion.div
-          className="p-4 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700"
+          className="p-4 bg-gray-300 rounded-lg"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2 }}
         >
-          <h3 className="text-xl font-semibold text-green-400 mb-3">
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">
             Profile Information
           </h3>
-          <p className="text-gray-300">Name: {user.name}</p>
-          <p className="text-gray-300">Email: {user.email}</p>
+          <p className="text-gray-600">Name: {user.name}</p>
+          <p className="text-gray-600">Email: {user.email}</p>
         </motion.div>
         <motion.div
-          className="p-4 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700"
+          className="p-4 bg-gray-300 rounded-lg"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.4 }}
         >
-          <h3 className="text-xl font-semibold text-green-400 mb-3">
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">
             Account Activity
           </h3>
-          <p className="text-gray-300">
+          <p className="text-gray-600">
             <span className="font-bold">Joined: </span>
             {new Date(user.createdAt).toLocaleDateString("en-US", {
               year: "numeric",
@@ -50,7 +58,7 @@ const DashboardPage = () => {
               day: "numeric",
             })}
           </p>
-          <p className="text-gray-300">
+          <p className="text-gray-600">
             <span className="font-bold">Last Login: </span>
 
             {formatDate(user.lastLogin)}
@@ -68,11 +76,13 @@ const DashboardPage = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
           onClick={handleLogout}
-          className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-				font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
-				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+          className="w-full py-2 px-4 bg-gray-500 hover:bg-gray-700 text-white font-semibold rounded-lg shadow-md transition duration-300 focus:ring-2 focus:ring-gray-600 focus:outline-none"
         >
-          Logout
+          {isLoading ? (
+            <Loader className="animate-spin text-white m-auto" />
+          ) : (
+            "Logout"
+          )}
         </motion.button>
       </motion.div>
     </motion.div>
